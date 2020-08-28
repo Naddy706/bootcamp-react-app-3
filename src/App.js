68c7804095e500f91f9e10f40a02a801878a0142
Naddy706 <nadirkhosa08@gmail.com>
2020-08-28 21:39:13 +0500

    Pass parametres in child component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import './App.css';
+import Dinner from './dinner.js';
 
 function App() {
+  var sweet="Barfi";
   return (
-   <h1>hello world deployed</h1>
+    <div className="App">
+    <Dinner dinnerItem="chicken Karahi" sweetdish="Gulab Jaman"/>
+    <hr />
+    <Dinner dinnerItem="chicken Biryani" sweetdish={sweet}/>
+    <hr/>
+    <Dinner dinnerItem="Nihari" sweetdish={sweet}/>
+    </div>
   );
 }
 
